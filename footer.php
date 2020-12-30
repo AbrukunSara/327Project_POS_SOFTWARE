ff525dfbbbf63540cf20d6abf5bba46afa565d42
AbrukunSara <sara.abrukun@northsouth.edu>
2020-12-31 00:11:12 +0600

    php files added

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="panel-footer"> </div>
+</div>
+</div>
+</div>
+<div style="text-align: center;"> <a href="" style="color: #0e1d3e;">DoryPOS SoftWare</a> © 2018 All Rights Reserved | Developed by <a style="color: #0e1d3e;" href="">North South| Dhaka
+Lab</a> </div>
+</div>
+
+</div>
+</body>
+</html>
