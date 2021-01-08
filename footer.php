2e0eb8d3097457d8d30190be264bbd2f03cef9af
AbrukunSara <58888164+AbrukunSara@users.noreply.github.com>
2021-01-09 00:21:11 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 </div>
 </div>
-<div style="text-align: center;"> <a href="" style="color: #0e1d3e;">DoryPOS SoftWare</a> © 2018 All Rights Reserved | Developed by <a style="color: #0e1d3e;" href="">North South| Dhaka
+<div style="text-align: center;"> <a href="" style="color: #0e1d3e;">DoryPOS SoftWare</a> © 2021 All Rights Reserved | Developed by <a style="color: #0e1d3e;" href="">North South| Dhaka
 Lab</a> </div>
 </div>
 
